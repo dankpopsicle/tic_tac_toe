fdc69ed7358a52f2e4ca7d59c6ae38bbfeaae5c0
Adam Taber <altaber@gmail.com>
2022-07-19 12:36:36 -0500

    split up methods

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,93 +1,106 @@
 class TicTacToe
   attr_reader :board
+  attr_reader :play_counter
 
-  @@play_counter = 0
-  
-  def initialize(board, player_input, player)
-    @board = board
-    @player_input = player_input
-    @player = player
-    @@play_counter += 1
+  def initialize
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @player_input = 0
+    @position = 1
+    @play_counter = 0
   end
 
-  def new_board
+  def play_game
+    print_board
+    until self.game_over?
+      player_one_move
+      game_end if self.game_over?
+      if self.game_over? != true
+        player_two_move
+        game_end if self.game_over?
+      end
+    end
+  end
+
+  def game_over?
+    full_board? || winner?("X") || winner?("O")
+  end
+
+  def full_board?
+    @play_counter == 9
+  end
+
+  def game_end
+    puts "It's a tie!" if full_board?
+    puts "Player one wins!" if winner?("X")
+    puts "Player two wins!" if winner?("O")
+  end
+
+  def winner?(symbol)
+    temp_board = @board.transpose
+    value = false
+    @board.each do |row|
+      if row.all?(symbol)
+        value = true
+        break
+      end
+    end
+    if value == false
+      temp_board.each do |row|
+        if row.all?(symbol)
+          value = true
+          break
+        end
+      end
+    end
+    value
+  end
+
+  def player_one_move
+    puts 'Player One: please enter a number from the board'
+    @player_input = get_player_input
+    new_board("X")
+    self.print_board
+    @play_counter += 1
+  end
+
+  def player_two_move
+    puts 'Player Two: please enter a number from the board'
+    @player_input = get_player_input
+    new_board("O")
+    self.print_board
+    @play_counter += 1
+  end
+
+  def get_player_input
+    player_input = gets.chomp.to_i
+    input_valid?(player_input) ? player_input : input_invalid
+  end
+
+  def input_valid?(player_input)
+    value = false
+    @board.each do |row|
+      if row.include?(player_input)
+        value = true
+      end
+    end
+    value
+  end
+
+  def input_invalid
+    puts "Please enter a number from the board"
+    get_player_input
+  end
+
+  def new_board(symbol)
     @board.each do |row|
       if row.include?(@player_input)
-        row[row.find_index(@player_input)] = @player
+        row[row.find_index(@player_input)] = symbol
       end
     end
     @board
   end
 
-  def game_over
-    @board.each do |row|
-      if row.all?("X")
-        puts "Player One is the winner!"
-        return true
-      elsif row.all?("O")
-        puts "Player Two is the winner!"
-        return true
-      elsif @@play_counter == 9
-        puts "It's a tie!"
-        return true
-      else false
-      end
-    end
-    @board.transpose.each do |column|
-      if column.all?("X")
-        puts "Player One is the winner!"
-        return true
-      elsif column.all?("O")
-        puts "Player Two is the winner!"
-        return true
-      elsif @@play_counter == 9
-        puts "It's a tie!"
-        return true
-      else false
-      end
-    end
-    if ((@board[0][0] == @board[1][1]) && (@board[1][1] == @board[2][2])) || ((@board[0][2] == @board[1][1]) && (@board[1][1] == @board[2][0])) && (@board[1][1] == "X")
-      puts "Player One is the winner!"
-      return true
-    elsif ((@board[0][0] == @board[1][1]) && (@board[1][1] == @board[2][2])) || ((@board[0][2] == @board[1][1]) && (@board[1][1] == @board[2][0])) && (@board[1][1] == "O")
-      puts "Player Two is the winner!"
-      return true
-    else false
-    end
+  def print_board
+    p @board[0], @board[1], @board[2]
   end
 end
-
-def play_game
-  board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  player_one = true
-  player = "X"
-  game_over = false
-  player_input = 0
-  p board[0], board[1], board[2]
-  loop do
-    player_input = 0
-    if player_one == true
-      player = "X"
-      player_one = false
-      puts "Player One: please enter a number"
-    else player = "O"
-      player_one = true
-      puts "Player Two: please enter a number"
-    end
-    until player_input > 0 && player_input < 10
-      player_input = gets.to_i
-      if player_input < 0 || player_input > 9
-        puts "please try a different number" 
-      end
-    end
-    round = TicTacToe.new(board, player_input, player)
-    board = round.new_board
-    p board[0], board[1], board[2]
-    game_over = round.game_over
-    if game_over == true
-      break
-    end
-  end
-end
-
-play_game
